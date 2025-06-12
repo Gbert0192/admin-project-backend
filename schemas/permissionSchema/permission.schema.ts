bdd6b3a451f8f89a58a6598547d655a03a3efa71
egipsinargo <egipsinargo123@gmail.com>
2025-06-12 11:58:39 +0700

    re-fix

--- schemas/permissionSchema/permission.schema.ts
+++ schemas/permissionSchema/permission.schema.ts
@@ -2,11 +2,8 @@ import { z } from "zod";
 
 export const permissionBodySchema = z.object({
   permission_name: z.string().min(1, "Permission name is required"),
-  route: z.array(
-    z.number()
-    // .min(1, "Route is required")
-    // .startsWith("/", "Route must start with /")
-  ),
+  route: z.string().min(1, "Route is required").startsWith("/", "Route must start with /")
+  
 });
 
 export const permissionParamsSchema = z.object({
